06e2a89994212e38b0165d76c7107602a24dbb90
Sidharth Panda <32598234+panda002@users.noreply.github.com>
2020-01-30 16:50:43 -0500

    Learning JS

---
+++ Creating a Server.js
@@ -0,0 +1,11 @@
+var http = require('http')
+
+var server = http.createServer(function (req, res) {
+    console.log('request was made: '+req.url)
+    res.writeHead(200,{'Content-Type': 'text/plain'})
+    res.end('hey Sidharth') // will be printed on browser on 127.0.0.1:3000
+
+})
+
+server.listen(3000, '127.0.0.1')
+console.log('Yo dawgs, now listening to port 3000')
